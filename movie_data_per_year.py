f4a52bb6b5bb38c6aff69fabed08117cbc010bd6
jhlipton <jeff.lipton@gmail.com>
2017-09-15 11:32:13 -0700

    Update movie_data_per_year.py

--- movie_data_per_year.py
+++ movie_data_per_year.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 import sys
 sys.__stdout__ = sys.stdout
 
+# Get data from the Web
+
 url = "https://raw.githubusercontent.com/sundeepblue/movie_rating_prediction/master/movie_metadata.csv"
 movie_data = pd.read_csv(url, encoding="Latin-1")
+
+# Create Pivot Tables
 
 mean_dur_by_year = movie_data.pivot_table(index="title_year", values="duration", aggfunc=numpy.mean)
 min_dur_by_year = movie_data.pivot_table(index="title_year", values="duration", aggfunc=numpy.min)
 max_dur_by_year = movie_data.pivot_table(index="title_year", values="duration", aggfunc=numpy.max)
+
+# Display the data
 
 plt.plot(mean_dur_by_year, "r")
 plt.plot(min_dur_by_year, "b")
